6e078ecf4ada84f63f1c98f2a5b72c7f4542f4c5
Eduardo Souza <eduardofidera@gmail.com>
2017-12-17 17:38:43 -0200

    init expensify app

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <div>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>home</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">edit</NavLink>
+        <NavLink to="/help" activeClassName="is-active">help</NavLink>
+        <NavLink to="/dasd" activeClassName="is-active">404</NavLink>
+    </div>
+);
+
+export default Header;
