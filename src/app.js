18570f3f52d6e488e7f3354dcdb324f295ef9c01
Eduardo Souza <eduardofidera@gmail.com>
2017-12-20 22:25:56 -0200

    added selector and improved redux architecture

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+
+import configureStore from './store/configureStore';
+
+import {addExpense, removeExpense, editExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+
+import getVisibleExpenses from './selectors/expenses';
+
+import expensesReducer from './reducers/expenses';
+import filtersReducer from './reducers/filters';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 200, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -1000 }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -165,6 +165,12 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     })
 };
 
@@ -193,16 +199,15 @@ store.subscribe(() => {
 
 
 
-const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 1000}));
-const expenseTwo = store.dispatch(addExpense({description: 'Lel', amount: 200, createdAt: -1000}));
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 21000}));
+const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 200, createdAt: -1000}));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense( expenseTwo.id, { amount: 500 } ));
 
-store.dispatch(setTextFilter());
 
-// store.dispatch(sortByAmount());
+store.dispatch(sortByAmount());
 
 // store.dispatch(sortByDate());
 
