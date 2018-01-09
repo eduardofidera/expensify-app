0741e74627c7ebc31690f0d6ec6c8200ac7dee8d
Eduardo Souza <eduardofidera@gmail.com>
2018-01-09 03:52:23 -0200

    created add/edit form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-        AddExpensePage
+        <h1>AddExpensePage</h1>
+        <ExpenseForm />
     </div>
 );
 
