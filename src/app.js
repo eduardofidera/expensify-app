3d5edb4a79a7cef8f6179f58cbcb7e72000a9721
Eduardo Souza <eduardofidera@gmail.com>
2018-01-09 02:09:14 -0200

    rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: -10
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-    store.dispatch(setTextFilter('rent'));
+    store.dispatch(setTextFilter('bill'));
 }, 3000)
 
 const state = store.getState();
