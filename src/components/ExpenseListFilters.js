6c4e0d731e9a8f30adea6a992eebf3e771793225
Eduardo Souza <eduardo@trezo.com.br>
2018-03-13 13:32:55 -0300

    added materialize colors, initiate dashboard styles

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -17,36 +17,45 @@ class ExpenseListFilters extends React.Component {
     render() {
         return (
             <div>
-                <input
-                type="text"
-                value={this.props.filters.text}
-                onChange={(e) => {
-                    this.props.dispatch(setTextFilter(e.target.value));
-                }}
-                />
-                <select
-                value={this.props.filters.sortBy}
-                onChange={(e) => {
-                    if (e.target.value === 'date') {
-                        this.props.dispatch(sortByDate());
-                    } else if (e.target.value === 'amount') {
-                        this.props.dispatch(sortByAmount());
-                    }
-                }}
-                >
-                    <option value="date">Date</option>
-                    <option value="amount">Amount</option>
-                </select>
-                <DateRangePicker
-                    startDate={this.props.filters.startDate}
-                    endDate={this.props.filters.endDate}
-                    onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                />
+                <h1>Expense List</h1>
+                <ul>
+                    <li>
+                        <input
+                        type="text"
+                        value={this.props.filters.text}
+                        onChange={(e) => {
+                            this.props.dispatch(setTextFilter(e.target.value));
+                        }}
+                        />
+                    </li>
+                    <li>
+                        <select
+                        value={this.props.filters.sortBy}
+                        onChange={(e) => {
+                            if (e.target.value === 'date') {
+                                this.props.dispatch(sortByDate());
+                            } else if (e.target.value === 'amount') {
+                                this.props.dispatch(sortByAmount());
+                            }
+                        }}
+                        >
+                            <option value="date">Date</option>
+                            <option value="amount">Amount</option>
+                        </select>
+                    </li>
+                    <li>
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}
+                            endDate={this.props.filters.endDate}
+                            onDatesChange={this.onDatesChange}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            showClearDates={true}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                        />
+                    </li>
+                </ul>
             </div>
         )
     }
